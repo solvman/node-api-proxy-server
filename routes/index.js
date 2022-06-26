854b314037b0aab81f41f2a81926e86fad9911d5
Sergey <solvman@gmail.com>
2022-06-25 22:51:04 -0500

    remove clg

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,6 @@ const axios = require("axios");
 router.get("/", async (req, res) => {
   try {
     const response = await axios("https://zenquotes.io/api/random");
-    console.log(response.data);
     res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ error: error.message });
