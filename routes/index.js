81897a769bcbd53a7ee6b438ba8d9be30c59cb9a
Sergey <solvman@gmail.com>
2022-06-25 23:19:26 -0500

    assecc controll added

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const axios = require("axios");
 router.get("/", async (req, res) => {
   try {
     const response = await axios("https://zenquotes.io/api/random");
+    res.header("Access-Control-Allow-Origin", "*");
     res.status(200).json(response.data);
   } catch (error) {
     res.status(500).json({ error: error.message });
